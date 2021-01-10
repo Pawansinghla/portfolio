848a6fb0bfafe4caf51cc8e94e7abd9cae2c712d
pawan singhla <pawansinghla300@gmail.com>
2021-01-10 14:46:31 +0530

    about

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(()=>{
+    $(window).scroll(()=>{
+        if(this.scrollY>20){
+            $('.navbar').addClass('sticky');
+
+        }
+        else{$('.navbar').removeClass('sticky');
+
+        }
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(()=>{
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
+
+});
